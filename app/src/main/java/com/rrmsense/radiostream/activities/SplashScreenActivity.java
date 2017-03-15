3f71b7b257728705d9b0e8a0c29936e230d773d2
sam43 <sayemruet.103043@gmail.com>
2017-03-15 14:14:55 +0600

    Splash screen with fading animation shimmer

--- app/src/main/java/com/rrmsense/radiostream/activities/SplashScreenActivity.java
+++ app/src/main/java/com/rrmsense/radiostream/activities/SplashScreenActivity.java
@@ -1,20 +1,44 @@
 package com.rrmsense.radiostream.activities;
 
+import android.animation.ObjectAnimator;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
+import com.facebook.shimmer.ShimmerFrameLayout;
 import com.rrmsense.radiostream.R;
 
 public class SplashScreenActivity extends AppCompatActivity {
-    private static int SPLASH_TIME_OUT = 1000;
+    private static int SPLASH_TIME_OUT = 6000;
     //ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+
+        TextView tx = (TextView)findViewById(R.id.loading_text);
+
+        Typeface custom_font = Typeface.createFromAsset(getAssets(),  "fonts/zektonBold.ttf");
+
+        tx.setTypeface(custom_font);
+
+        ShimmerFrameLayout container =
+                (ShimmerFrameLayout) findViewById(R.id.shimmer_view_container);
+        container.setBaseAlpha(0.25f);
+        container.setDuration(1600);
+        //container.setDropoff(0.1f);
+        //container.useDefaults();
+        container.setRepeatMode(ObjectAnimator.REVERSE);
+        //container.setRepeatCount(6);
+        //container.setTilt(0);
+        //container.setIntensity(0.1f);
+        //container.setMaskShape(ShimmerFrameLayout.MaskShape.RADIAL);
+        container.startShimmerAnimation();
 
         //image = (ImageView) findViewById(R.id.image);
 
